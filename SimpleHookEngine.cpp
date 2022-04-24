de7dc6fb5ab6d79435fd9ce5b2d7b34866c35d2a
iyzyi <49014178+iyzyi@users.noreply.github.com>
2022-04-24 12:39:47 +0800

    ..

--- SimpleHookEngine.cpp
+++ SimpleHookEngine.cpp
@@ -3,20 +3,15 @@
 
 
 typedef int
-(WINAPI* PFN_MessageBoxA)(
-    HWND hWnd,
-    LPCTSTR lpText,
-    LPCTSTR lpCaption,
-    UINT uType
-    );
+(WINAPI* PFN_MessageBoxA)(HWND hWnd, LPCTSTR lpText, LPCTSTR lpCaption, UINT uType);
 
-PFN_MessageBoxA TrueMessageBoxA = (PFN_MessageBoxA)GetProcAddress(GetModuleHandle(L"user32"), "MessageBoxA");
+PFN_MessageBoxA TrueMessageBoxA = (PFN_MessageBoxA)GetProcAddress(GetModuleHandleA("user32"), "MessageBoxA");
 
 int WINAPI MyMessageBoxA(HWND hWnd, LPCTSTR lpText, LPCTSTR lpCaption, UINT uType)
 {
     int ret;
     char newText[1024] = { 0 };
-    char newCaption[256] = "iyzyi";
+    char newCaption[] = "iyzyi";
     strcpy_s(newText, (char*)lpText);
     strcat_s(newText, "\nMessageBoxA Hacked by iyzyi!");//篡改消息框内容
     uType |= MB_ICONERROR;							
@@ -28,11 +23,11 @@ int WINAPI MyMessageBoxA(HWND hWnd, LPCTSTR lpText, LPCTSTR lpCaption, UINT uTyp
 int wmain(int argc, WCHAR* argv[]) {
 	MessageBoxA(NULL, "测试文本1", "测试1", NULL);
 
-	InstallHook((PVOID*)&TrueMessageBoxA, MyMessageBoxA);
+	InstallHook((void**)&TrueMessageBoxA, MyMessageBoxA);
 
 	MessageBoxA(NULL, "测试文本2", "测试2", NULL);
 
-    UninstallHook((PVOID*)&TrueMessageBoxA);
+    UninstallHook((void**)&TrueMessageBoxA);
 
     MessageBoxA(NULL, "测试文本3", "测试3", NULL);
 }
