ad53b589f84303afd118b2172b00be584b8b072f
iyzyi <49014178+iyzyi@users.noreply.github.com>
2022-05-02 22:03:32 +0800

    注释

--- TestUI/CChooseProcess.h
+++ TestUI/CChooseProcess.h
@@ -18,6 +18,8 @@ struct _ListRowData {
 	}
 };
 
+// 数据都在本类里，ListCtrl只负责绘制。
+// 只有数据CListData和绘制CListCtrl分离，才能实现ListCtrl的筛选数据再绘制。
 class CListData {
 private:
 	CListCtrl* pListCtrl = NULL;
@@ -27,13 +29,10 @@ private:
 
 public:
 	CListData(CListCtrl* pListCtrl);
-
 	~CListData();
-
 
 	// 添加一行数据（不是向ListCtrl中添加，而是向我们自定义的CListData类中添加）
 	void AddRow(CString* pcsPID, CString* pcsProcName, CString* pcsProcWinText, CString* pcsProcPath, CString* pcsProcCmdLine);
-
 
 	// 向ListCtrl中绘制我们自定义的CListData类中的所有行的数据（本函数不负责排序）
 	void DisplayData();
@@ -44,11 +43,17 @@ public:
 	// 向ListCtrl中绘制我们自定义的CListData类中的数据（经过了关键词的过滤）（本函数不负责排序）
 	void DisplayFilterData(CString csFilterKeyword);
 
+	// 按列排序
 	void SortDataByCol(DWORD dwCol);
+
+	// 检查此前设置的是按第几列排序（根据表头是否有▲or▼)
 	int CheckSortColNum();
+
+	// 改变某一列默认的排序方式，并进行排序（▲升序▼降序互转）
 	void ChangeSortType(DWORD dwCol);
 };
 
+// 进行ListCtrl排序的回调函数
 int CALLBACK ListCompare(LPARAM lParam1, LPARAM lParam2, LPARAM lpSortData);
 
 
