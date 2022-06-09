ece0d06d0d1308e6bdfd35154120385777913382
iyzyi <49014178+iyzyi@users.noreply.github.com>
2022-06-10 00:44:08 +0800

    改用bat复制BypassUAC.dll

---
+++ BypassUAC/CopyDllBypassUAC.bat
@@ -0,0 +1,3 @@
+echo f | xcopy /f/y  "..\Win32\Release\BypassUAC.dll" "..\x64\Release\BypassUAC.dll"
+echo f | xcopy /f/y  "..\Win32\Release\BypassUAC.dll" "..\x64\Debug\BypassUAC.dll"
+echo f | xcopy /f/y  "..\Win32\Release\BypassUAC.dll" "..\Win32\Debug\BypassUAC.dll"
